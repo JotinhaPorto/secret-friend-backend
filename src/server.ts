d2010ae003c6ccc3b10d108ed5b544f8cf6f57bc
João Pedro Braga Porto <joao.braga.porto01@gmail.com>
2023-12-24 06:13:10 -0300

    Creation production server

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,10 @@ import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
 import http from 'http'
+import https from 'https'
 import adminRoutes from './routes/admin'
 import siteRoutes from './routes/site'
+import fs from 'fs'
 
 const app = express()
 
@@ -23,7 +25,19 @@ const runServer = (port: number, server: http.Server) => {
 const devServer = http.createServer(app)
 
 if (process.env.NODE_ENV === 'production') {
-    //
+    //Configurar certificado: gero dois arquivos no servidor e referencio eles aqui
+    const options = {
+        key: fs.readFileSync(process.env.SSL_KEY as string),
+        cert: fs.readFileSync(process.env.SSL_CERT as string)
+    }
+
+    const secServer = https.createServer(options, app)
+    
+    //Roda em ambiente de desenvolvimento
+    runServer(80, devServer)
+    
+    //Roda em ambiente de produção
+    runServer(443, secServer)
 }
 
 else {
